ce49b94bbfeb5495ec6e2c147d0357d69e0d6118
Sworly21 <Sworly21@users.noreply.github.com>
2022-09-21 08:09:54 +0000

    deploy: 86a4010f3ecb4a376b982955f42876d408e22c6c

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ function szamol(){
     let magassag = parseFloat(document.getElementById('magassag').value);
     let suly = parseFloat(document.getElementById('suly').value);
 
-    document.getElementById('BMIertek').textContent = Math.round(suly / (magassag / 100)**2);
+    
+   let BMIertek = document.getElementById('BMIertek').textContent = Math.round(suly / (magassag / 100)**2);
 
-    if(BMIertek <= 18.5 ){
+    document.getElementById('sovany').style.backgroundColor = 'transparent';
+    document.getElementById('normal').style.backgroundColor = 'transparent';
+    document.getElementById('tulsuly').style.backgroundColor = 'transparent';
+    document.getElementById('1tulsuly').style.backgroundColor = 'transparent';
+    document.getElementById('2tulsuly').style.backgroundColor = 'transparent';
+    document.getElementById('3tulsuly').style.backgroundColor = 'transparent';
+    
+    if(BMIertek < 18.5 ){
         document.getElementById('sovany').style.backgroundColor = 'orange';
+    }else if(BMIertek >= 18.5 && BMIertek <= 24.9){
+        document.getElementById('normal').style.backgroundColor = 'orange';
+    }else if(BMIertek >= 25 && BMIertek <= 29.9){
+        document.getElementById('tulsuly').style.backgroundColor = 'orange';
+    }else if(BMIertek >= 30 && BMIertek <= 34.9){
+        document.getElementById('1tulsuly').style.backgroundColor = 'orange';
+    }else if(BMIertek >= 35 && BMIertek <= 39.9){
+        document.getElementById('2tulsuly').style.backgroundColor = 'orange';
+    }else{
+        document.getElementById('3tulsuly').style.backgroundColor = 'orange';
     }
 
 }
